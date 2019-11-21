michelhe/rustboyadvance-ng

commit e885e9ff1393dd14f5e4132c71a7641d658399cb

Fix accesses outside of IO region

src/core/iodev.rs
@@ -54,6 +54,9 @@ impl Bus for IoDevices {
     fn read_16(&self, addr: Addr) -> u16 {
         let io = self;
         let io_addr = addr + IO_BASE;
+        if addr > 0x0800 {
+            return 0;
+        }
         match io_addr {
             REG_DISPCNT => io.gpu.dispcnt.0,
             REG_DISPSTAT => io.gpu.dispstat.0,
@@ -99,12 +102,12 @@ impl Bus for IoDevices {
             REG_HALTCNT => 0,
             REG_KEYINPUT => io.keyinput as u16,
             _ => {
-                println!(
-                    "Unimplemented read from {:x} {}",
-                    io_addr,
-                    io_reg_string(io_addr)
-                );
-                io.mem.read_16(addr)
+                // println!(
+                //     "Unimplemented read from {:x} {}",
+                //     io_addr,
+                //     io_reg_string(io_addr)
+                // );
+                0
             }
         }
     }
@@ -125,7 +128,9 @@ impl Bus for IoDevices {
 
     fn write_16(&mut self, addr: Addr, value: u16) {
         let mut io = self;
-        io.mem.write_16(addr, value);
+        if addr > 0x0800 {
+            return;
+        }
         let io_addr = addr + IO_BASE;
         match io_addr {
             REG_DISPCNT => io.gpu.dispcnt.0 = value,
@@ -241,11 +246,11 @@ impl Bus for IoDevices {
                 }
             }
             _ => {
-                println!(
-                    "Unimplemented write to {:x} {}",
-                    io_addr,
-                    io_reg_string(io_addr)
-                );
+                // println!(
+                //     "Unimplemented write to {:x} {}",
+                //     io_addr,
+                //     io_reg_string(io_addr)
+                // );
             }
         }
     }

commit e258903d8996c5d8030481ee3c4cdca5572e8fcc

Fix bios infinite loop caused by changes in e885e9ff1393dd14f5e4132c71a7641d658399cb
This commit removed the dummy iomem buffer, and the bios busy waits on
REG_SOUNDBIAS in order to proceed.

src/core/iodev.rs
@@ -26,6 +26,7 @@ pub struct IoDevices {
     pub post_boot_flag: bool,
     pub waitcnt: WaitControl, // TODO also implement 4000800
     pub haltcnt: HaltState,
+    pub sound_bias: u16,
 
     mem: BoxedMemory,
 }
@@ -42,6 +43,7 @@ impl IoDevices {
             haltcnt: HaltState::Running,
             keyinput: keypad::KEYINPUT_ALL_RELEASED,
             waitcnt: WaitControl(0),
+            sound_bias: 0x200,
         }
     }
 }
@@ -95,6 +97,8 @@ impl Bus for IoDevices {
             REG_DMA1CNT_H => io.dmac.channels[1].ctrl.0,
             REG_DMA2CNT_H => io.dmac.channels[2].ctrl.0,
             REG_DMA3CNT_H => io.dmac.channels[3].ctrl.0,
+
+            REG_SOUNDBIAS => io.sound_bias,
 
             REG_WAITCNT => io.waitcnt.0,
 
@@ -233,6 +237,8 @@ impl Bus for IoDevices {
                 let channel_id = (ofs / 12) as usize;
                 io.dmac.write_16(channel_id, ofs % 12, value)
             }
+
+            REG_SOUNDBIAS => io.sound_bias = value & 0xc3fe,
 
             REG_WAITCNT => io.waitcnt.0 = value,
 