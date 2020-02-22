e282f12b7f93a88d1328b91f4613ded193e68ffe
Michel Heily <michelheily@gmail.com>
2020-02-23 00:19:55 +0200

    refactor: split "plat" binaries into separate crates

--- rustboyadvance-sdl2/build.rs
+++ rustboyadvance-sdl2/build.rs
@@ -1,9 +1,12 @@
 #[cfg(windows)]
+use winres;
+
+#[cfg(windows)]
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("assets/icon.ico");
+    res.set_icon("../assets/icon.ico");
     res.compile().unwrap();
 }
 
-#[cfg(unix)]
+#[cfg(not(windows))]
 fn main() {}
