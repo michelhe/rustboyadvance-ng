9af2d30524ca7fcdcec2dd10e3b65681554c477d
Michel Heily <michelheily@gmail.com>
2020-04-13 20:12:08 +0300

    web: Add logo image

--- platform/rustboyadvance-wasm/app/webpack.config.js
+++ platform/rustboyadvance-wasm/app/webpack.config.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin(['index.html', '../../../assets/icon.svg'])
   ],
 };
