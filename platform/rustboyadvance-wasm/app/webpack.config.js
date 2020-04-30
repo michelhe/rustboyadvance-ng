915c0db6d7c3d2a8e571cc07b6505ad7952b0a54
Michel Heily <michelheily@gmail.com>
2020-04-30 09:59:13 +0300

    Add favicon to wasm app

--- platform/rustboyadvance-wasm/app/webpack.config.js
+++ platform/rustboyadvance-wasm/app/webpack.config.js
@@ -1,4 +1,6 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
 const path = require('path');
 
 module.exports = {
@@ -9,6 +11,24 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html', '../../../assets/icon.svg'])
+    new CopyWebpackPlugin(['index.html', '../../../assets/icon.png']),
+    new FaviconsWebpackPlugin({
+      logo: '../../../assets/icon.png',
+      cache: true,
+      mode: 'webapp',
+      devMode: 'webapp',
+      favicons: {
+        appName: 'rustboyadvance',
+        appDescription: 'Web Demo for rustboyadvance',
+        developerName: 'michel',
+        developerURL: null, // prevent retrieving from the nearest package.json
+        background: '#ddd',
+        theme_color: '#333',
+        icons: {
+          coast: false,
+          yandex: false,
+        }
+      }
+    })
   ],
 };
