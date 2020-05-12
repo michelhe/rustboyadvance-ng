michelhe/rustboyadvance-ng

commit fe172bedc6bf70b1022f419986585981bc9f3642

Fix busy waiting in wait_for_rom() by using a blocking event iterator
Also move the redraw code into a closure to be able to call it from
two places. By the result of this waiting for ROMs no longer consumes
100% of a CPU core.

platform/rustboyadvance-sdl2/src/main.rs
@@ -1,5 +1,5 @@
 use sdl2;
-use sdl2::event::Event;
+use sdl2::event::{Event, WindowEvent};
 use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Scancode;
 use sdl2::messagebox::*;
@@ -63,16 +63,8 @@ fn wait_for_rom(canvas: &mut WindowCanvas, event_pump: &mut EventPump) -> Result
         .load_texture("assets/icon_cropped_small.png")
         .expect("failed to load icon");
     let background = Color::RGB(0xDD, 0xDD, 0xDD);
-    loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::DropFile { filename, .. } => {
-                    return Ok(filename);
-                }
-                Event::Quit { .. } => process::exit(0),
-                _ => {}
-            }
-        }
+
+    let mut redraw = || -> Result<(), String> {
         canvas.set_draw_color(background);
         canvas.clear();
         canvas.copy(
@@ -85,6 +77,25 @@ fn wait_for_rom(canvas: &mut WindowCanvas, event_pump: &mut EventPump) -> Result
             )),
         )?;
         canvas.present();
+        Ok(())
+    };
+
+    redraw()?;
+
+    loop {
+        for event in event_pump.wait_iter() {
+            match event {
+                Event::DropFile { filename, .. } => {
+                    return Ok(filename);
+                }
+                Event::Quit { .. } => process::exit(0),
+                Event::Window { win_event, .. } => match win_event {
+                    WindowEvent::SizeChanged(..) => redraw()?,
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
     }
 }
 

commit 07f4360bc298c38a7d922517949569193b70ca53

sdl2: Window needs to be redrawn when restoring it from the taskbar
Fixes regression introduced by fe172bedc6bf70b1022f419986585981bc9f3642

platform/rustboyadvance-sdl2/src/main.rs
@@ -91,7 +91,7 @@ fn wait_for_rom(canvas: &mut WindowCanvas, event_pump: &mut EventPump) -> Result
                 }
                 Event::Quit { .. } => process::exit(0),
                 Event::Window { win_event, .. } => match win_event {
-                    WindowEvent::SizeChanged(..) => redraw()?,
+                    WindowEvent::SizeChanged(..) | WindowEvent::Restored => redraw()?,
                     _ => {}
                 },
                 _ => {}