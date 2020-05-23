d6b20e6593dbeb806d99ac4d5d90790769c4bbb3
michelhe <michelhe@users.noreply.github.com>
2020-05-23 08:50:37 +0000

    deploy: 8a562135e0f07863c09b951c73f00754f08e2b01

--- bootstrap.js
+++ bootstrap.js
@@ -243,7 +243,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rustboyadvance_wasm_bg.wasm":"cdb3b438ca898d286a07"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rustboyadvance_wasm_bg.wasm":"8ea8b4836b99cdcab723"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
