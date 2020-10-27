5645a025038e839a0a09f774dd4142146d690a6e
Michel Heily <michelheily@gmail.com>
2020-10-27 02:02:53 +0200

    android: Fix takePersistableUriPermission() error on Android 10
    Used wrong Intent ACTION

--- platform/android/app/src/main/java/com/mrmichel/rustdroid_emu/ui/library/RomListActivity.java
+++ platform/android/app/src/main/java/com/mrmichel/rustdroid_emu/ui/library/RomListActivity.java
@@ -246,7 +246,7 @@ public class RomListActivity extends AppCompatActivity {
     }
 
     void doImportRom() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
         Log.d(TAG, "pressed import rom");
