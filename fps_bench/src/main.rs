4c8589e193243b30288038f7d3b88fa29f701a6e
Michel Heily <michelheily@gmail.com>
2022-09-12 01:14:22 +0300

    Fix missing linking against utils in fps_bench

--- fps_bench/src/main.rs
+++ fps_bench/src/main.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::rc::Rc;
 
 use rustboyadvance_core::prelude::*;
-use rustboyadvance_core::util::FpsCounter;
+use rustboyadvance_utils::FpsCounter;
 
 struct DummyAudio {}
 
