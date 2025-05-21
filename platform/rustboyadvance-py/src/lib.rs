2cd07aeefef6e177fe3b299c64984a78a8d65910
wissammm <wissamboussella@gmail.com>
2025-05-21 16:41:30 +0200

    python library working with maturin develop --release, load not yet tested

--- platform/rustboyadvance-py/src/lib.rs
+++ platform/rustboyadvance-py/src/lib.rs
@@ -1,14 +1,36 @@
 use pyo3::prelude::*;
 use rustboyadvance_core::prelude::*;
+use std::fs::read;
+
+#[pymodule]
+fn rustboyadvance_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<RustGba>()?;
+    Ok(())
+}
 
 #[pyclass(unsendable)]
 pub struct RustGba {
+    core: Option<GameBoyAdvance>,
 }
 
 #[pymethods]
 impl RustGba {
     #[new]
     pub fn new() -> Self {
-        
+        RustGba { core: None }
+    }
+
+    pub fn load(&mut self, bios_path: &str, rom_path: &str) -> PyResult<()> {
+        let bios = read(bios_path)
+            .map_err(|e| pyo3::exceptions::PyIOError::new_err(format!("Failed to read BIOS: {e}")))?;
+        let rom = read(rom_path)
+            .map_err(|e| pyo3::exceptions::PyIOError::new_err(format!("Failed to read ROM: {e}")))?;
+
+        let bios = bios.into_boxed_slice();
+        let cartridge = GamepakBuilder::new().buffer(&rom).build().unwrap();
+        let audio = NullAudio::new();
+
+        self.core = Some(GameBoyAdvance::new(bios, cartridge, audio));
+        Ok(())
     }
 }
