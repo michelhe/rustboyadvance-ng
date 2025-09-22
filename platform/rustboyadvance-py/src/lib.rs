90dccf32a12b9c7a6e232cb60c2265246728bd62
greg-is-kub <greg_is_kub@pm.me>
2025-09-22 16:14:32 +0200

    refactor (rustboy-py): new add_stop_addr, now takes a vector (list) as input

--- platform/rustboyadvance-py/src/lib.rs
+++ platform/rustboyadvance-py/src/lib.rs
@@ -85,11 +85,16 @@ impl RustGba {
             Err(pyo3::exceptions::PyRuntimeError::new_err("GBA core not loaded"))
         }
     }
-
-    fn add_stop_addr(&mut self, addr:u32, value:i16, is_active:bool , name:String, id:u32) -> PyResult<()> {
+    
+    fn add_stop_addr(&mut self, stop_addresses : Vec<(u32,i16,bool,String,u32)> ) -> PyResult<()> {
         if let Some(core) = &mut self.core {
-            debug!("Adding stop address: addr={}, value={}, is_active={}, name={}, id={}\n", &addr, &value, &is_active, &name,id);
-            core.add_stop_addr(addr, value, is_active, name, id);
+           for (addr, value, is_active, name, id) in stop_addresses {
+                debug!(
+                    "Adding stop address: addr={}, value={}, is_active={}, name={}, id={}",
+                    addr, value, is_active, name, id
+                );
+                core.add_stop_addr(addr, value, is_active, name, id);
+            }
             Ok(())
         } else {
             Err(pyo3::exceptions::PyRuntimeError::new_err("GBA core not loaded"))
